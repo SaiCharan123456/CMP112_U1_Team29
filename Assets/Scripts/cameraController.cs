be481263a031fc0e364624cd4c368bf3d2d0381d
Luke <lukehay01@gmail.com>
2025-11-03 16:00:03 +0000

    Camera 2nd Attempt
    Added new code to try a new approach to the camera. Camera still stutters when trying to rotate it, will likely need further intervention as the lookAt components have been removed, which I thought were the problem before.

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -3,30 +3,34 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    private Vector3 offset;
-    private float xRotation;
-    private float yRotation;
+	[SerializeField] private GameObject playerAnchor;
+	[SerializeField] private float sensitivity = 1;
+	private Vector3 offset;
+	private float xRotation;
+	private float yRotation;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		offset = transform.position - playerAnchor.transform.position;
+	}
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        Vector3 rotationChange = new Vector3(xRotation, yRotation, 0);
-        transform.position = player.transform.position + offset + rotationChange;
-    }
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		//Vector3 rotationChange = new Vector3(xRotation, yRotation, 0);
+		//transform.position = player.transform.position + offset + rotationChange;
+		
+		transform.rotation = Quaternion.AngleAxis(xRotation * sensitivity, Vector3.up);
+		transform.rotation = Quaternion.AngleAxis(yRotation * sensitivity, Vector3.right);
+	}
 
-    void OnLook(InputValue value)
-    {
-        Vector2 lookAngle = value.Get<Vector2>();
-        xRotation = lookAngle.x;
-        yRotation = lookAngle.y;
-        
-    }
+	void OnLook(InputValue value)
+	{
+		Vector2 lookAngle = value.Get<Vector2>();
+		xRotation = lookAngle.x;
+		yRotation = lookAngle.y;
+		
+	}
 
 }
